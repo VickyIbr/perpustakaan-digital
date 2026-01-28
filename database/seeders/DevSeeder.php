b679f5a001ae45eae1ae5ff78294ec487ef19cd9
VickyIbr <vickyibrahimovic.id@gmail.com>
2026-01-28 07:23:51 +0700

    Initial Commit

---
+++ database/seeders/DevSeeder.php
@@ -0,0 +1,202 @@
+<?php
+
+namespace Database\Seeders;
+
+use Carbon\Carbon;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+
+class DevSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        DB::table('books')->insert([
+            [
+                'id' => 1,
+                'book_code' => 'BK001',
+                'title' => 'Laskar Pelangi',
+                'author' => 'Andrea Hirata',
+                'publisher' => 'Bentang Pustaka',
+                'year' => 2005,
+                'created_at' => now(),
+                'updated_at' => now(),
+            ],
+            [
+                'id' => 2,
+                'book_code' => 'BK002',
+                'title' => 'Bumi Manusia',
+                'author' => 'Pramoedya Ananta Toer',
+                'publisher' => 'Hasta Mitra',
+                'year' => 1980,
+                'created_at' => now(),
+                'updated_at' => now(),
+            ],
+            [
+                'id' => 3,
+                'book_code' => 'BK003',
+                'title' => 'Negeri 5 Menara',
+                'author' => 'Ahmad Fuadi',
+                'publisher' => 'Gramedia',
+                'year' => 2009,
+                'created_at' => now(),
+                'updated_at' => now(),
+            ],
+            [
+                'id' => 4,
+                'book_code' => 'BK004',
+                'title' => 'Ayat-Ayat Cinta',
+                'author' => 'Habiburrahman El Shirazy',
+                'publisher' => 'Republika',
+                'year' => 2004,
+                'created_at' => now(),
+                'updated_at' => now(),
+            ],
+            [
+                'id' => 5,
+                'book_code' => 'BK005',
+                'title' => 'Harry Potter and the Philosopher\'s Stone',
+                'author' => 'J.K. Rowling',
+                'publisher' => 'Bloomsbury',
+                'year' => 1997,
+                'created_at' => now(),
+                'updated_at' => now(),
+            ],
+            [
+                'id' => 6,
+                'book_code' => 'BK006',
+                'title' => 'The Hobbit',
+                'author' => 'J.R.R. Tolkien',
+                'publisher' => 'George Allen & Unwin',
+                'year' => 1937,
+                'created_at' => now(),
+                'updated_at' => now(),
+            ],
+            [
+                'id' => 7,
+                'book_code' => 'BK007',
+                'title' => '1984',
+                'author' => 'George Orwell',
+                'publisher' => 'Secker & Warburg',
+                'year' => 1949,
+                'created_at' => now(),
+                'updated_at' => now(),
+            ],
+            [
+                'id' => 8,
+                'book_code' => 'BK008',
+                'title' => 'Animal Farm',
+                'author' => 'George Orwell',
+                'publisher' => 'Secker & Warburg',
+                'year' => 1945,
+                'created_at' => now(),
+                'updated_at' => now(),
+            ],
+            [
+                'id' => 9,
+                'book_code' => 'BK009',
+                'title' => 'Dilan 1990',
+                'author' => 'Pidi Baiq',
+                'publisher' => 'Pastel Books',
+                'year' => 2014,
+                'created_at' => now(),
+                'updated_at' => now(),
+            ],
+            [
+                'id' => 10,
+                'book_code' => 'BK010',
+                'title' => 'Perahu Kertas',
+                'author' => 'Dee Lestari',
+                'publisher' => 'Bentang Pustaka',
+                'year' => 2009,
+                'created_at' => now(),
+                'updated_at' => now(),
+            ],
+            [
+                'id' => 11,
+                'book_code' => 'BK011',
+                'title' => 'The Alchemist',
+                'author' => 'Paulo Coelho',
+                'publisher' => 'HarperOne',
+                'year' => 1988,
+                'created_at' => now(),
+                'updated_at' => now(),
+            ],
+            [
+                'id' => 12,
+                'book_code' => 'BK012',
+                'title' => 'To Kill a Mockingbird',
+                'author' => 'Harper Lee',
+                'publisher' => 'J.B. Lippincott & Co.',
+                'year' => 1960,
+                'created_at' => now(),
+                'updated_at' => now(),
+            ],
+            [
+                'id' => 13,
+                'book_code' => 'BK013',
+                'title' => 'Pulang',
+                'author' => 'Tere Liye',
+                'publisher' => 'Republika',
+                'year' => 2015,
+                'created_at' => now(),
+                'updated_at' => now(),
+            ],
+            [
+                'id' => 14,
+                'book_code' => 'BK014',
+                'title' => 'Sang Pemimpi',
+                'author' => 'Andrea Hirata',
+                'publisher' => 'Bentang Pustaka',
+                'year' => 2006,
+                'created_at' => now(),
+                'updated_at' => now(),
+            ],
+            [
+                'id' => 15,
+                'book_code' => 'BK015',
+                'title' => 'The Da Vinci Code',
+                'author' => 'Dan Brown',
+                'publisher' => 'Doubleday',
+                'year' => 2003,
+                'created_at' => now(),
+                'updated_at' => now(),
+            ],
+        ]);
+
+        DB::table('users')->insert([
+            [
+                'id' => 1,
+                'school_id' => 539231357,
+                'name' => 'VICKY IBRAHIMOVIC',
+                'class' => 'XII TEL 8',
+                'major' => 'PPLG',
+                'username' => 'vickyibr',
+                'role' => 'member',
+                'password' => '$2y$12$H8UpG63vNgpdPGEkTuXEcuxXDmV9WoWaLxxwR2dAa8cGSQ1/p7/si',
+                'email' => '539231357@student.smktelkom-jkt.sch.id',
+                'email_verified_at' => null,
+                'remember_token' => null,
+                'created_at' => Carbon::parse('2026-01-19 06:36:11'),
+                'updated_at' => Carbon::parse('2026-01-19 06:36:11'),
+            ],
+            [
+                'id' => 2,
+                'school_id' => 0,
+                'name' => 'PERPUSTAKAAN',
+                'class' => '',
+                'major' => '',
+                'username' => 'admin_perpustakaan',
+                'role' => 'admin',
+                'password' => '$2y$12$RhmDicakTVWf7imnpGNWS.J1j19rctbqlh5qlCSQdlm8.LsGzTTFy',
+                'email' => '000000000@student.smktelkom-jkt.sch.id',
+                'email_verified_at' => null,
+                'remember_token' => null,
+                'created_at' => Carbon::parse('2026-01-20 01:14:57'),
+                'updated_at' => Carbon::parse('2026-01-20 01:14:57'),
+            ],
+        ]);
+    }
+}
